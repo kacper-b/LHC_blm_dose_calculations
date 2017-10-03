eaa37163e6e27e7b01ae10a87b51e6b3c004ad31
kbilko <kacper.bilko@cern.ch>
2017-10-03 17:56:15 +0200

    integration using pre and post offset

--- BLMDoseCalc.py
+++ BLMDoseCalc.py
@@ -11,49 +11,24 @@ class BLMDoseCalc:
         self.offset_post = 0
 
     def integrate_over_intensity_interval(self, intensity_interval):
-        integration_result, integration_result_pre_oc = 0, 0
+        integration_result_post_oc, integration_result_pre_oc = 0, 0
         try:
             blm_beam_on_data = self.data[(intensity_interval.start <= self.data.index) & (self.data.index <= intensity_interval.end)]
             offset_pre,_ = self.offset_calc_pre(intensity_interval)
             offset_post,_ = self.offset_calc_post(intensity_interval)
 
             integration_result_pre_oc = self.__integrate_oc(intensity_interval, blm_beam_on_data - offset_pre)
-            integration_result = self.__integrate(intensity_interval)
+            integration_result_post_oc = self.__integrate_oc(intensity_interval, blm_beam_on_data - offset_post)
 
-            rst = integration_result - offset_pre * (min(intensity_interval.end, blm_beam_on_data.index[-1]) - max(intensity_interval.start, blm_beam_on_data.index[0]))
-            rst = rst if rst >0 else 0
-            eq = abs(rst - integration_result_pre_oc) < 1e-15
-            if not eq:
-                print(rst- integration_result_pre_oc)
         except (IntegrationResultBelowZero, IntensityIntervalNotCoveredByBLMData,
                 NoBLMDataForIntensityInterval, PreOffsetEmpty, PostOffsetNan, PreOffsetNan,
                 PostOffsetEmpty, PreOffsetBelowZero, PostOffsetBelowZero) as e:
             # print(e)
             pass
-        else:
-            pass
-
-        return integration_result
-
-    def __integrate(self, intensity_interval):
-        beam_period = (intensity_interval.start <= self.data.index) & (self.data.index <= intensity_interval.end)
-        data = self.data[beam_period]
-
-        # is_whole_period_covered_by_data = (intensity_interval.start >= self.data.index[0]) & (self.data.index[-1] >= intensity_interval.end)
-
-        if not data.empty:# and is_whole_period_covered_by_data:
-            integral = np.trapz(y=data[data.columns[0]], x=data.index)
-            # if integral < 0:
-            #     raise IntegrationResultBelowZero('{} integrated dose < 0: {}'.format(self.name, intensity_interval))
-            return integral
-
-        # elif not is_whole_period_covered_by_data:
-        #     raise IntensityIntervalNotCoveredByBLMData('intensity interval does not cover {}: {}'.format(self.name, intensity_interval))
-        else:
-            raise NoBLMDataForIntensityInterval('{} dataframe for given intensity interval is empty: {}'.format(self.name, intensity_interval))
+        return integration_result_pre_oc
 
     def __integrate_oc(self, intensity_interval, data):
-        if not data.empty:# and is_whole_period_covered_by_data:
+        if not data.empty:
             integral = np.trapz(y=data[data.columns[0]], x=data.index)
             if integral < 0:
                 raise IntegrationResultBelowZero('{} integrated dose < 0: {}'.format(self.name, intensity_interval))
@@ -69,8 +44,6 @@ class BLMDoseCalc:
         if not offset_interval_pre.empty:
             offset = np.mean(offset_interval_pre[self.name])
             if not np.isnan(offset):
-                # if offset < 0:
-                #     raise PreOffsetBelowZero('{} pre-offset dataframe is empty: {}'.format(self.name, intensity_interval))
                 return offset, offset_interval_pre
             else:
                 raise PreOffsetNan('{} pre-offset dataframe is empty: {}'.format(self.name, intensity_interval))
@@ -84,8 +57,6 @@ class BLMDoseCalc:
         if not offset_interval_post.empty:
             offset = np.mean(offset_interval_post[self.name])
             if not np.isnan(offset):
-                # if offset < 0:
-                #     raise PostOffsetBelowZero('Post-offset value is < 0: {} {}'.format(self.name, intensity_interval))
                 return offset, offset_interval_post
             else:
                 raise PostOffsetNan('Post-offset value is Nan: {} {}'.format(self.name, intensity_interval))
