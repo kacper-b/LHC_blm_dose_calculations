1f76b7680277db169f595fa34ab2810314f00127
kbilko <kacper.bilko@cern.ch>
2017-12-19 16:18:02 +0100

    merge with common_classes repo

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 import sys
 import re
 
-PARENT_DIR_PATH = '/media/sf_monitoring_analysis'
+PARENT_DIR_PATH = '/home/monitoring_analysis'
 
 sys.path.append(PARENT_DIR_PATH)
 DATA_DIR_PATH = os.path.join(PARENT_DIR_PATH, 'data')
@@ -13,7 +13,7 @@ __RESULT_DIR_NAME = 'results'
 RESULT_DIR_PATH = os.path.join(PARENT_DIR_PATH, __RESULT_DIR_NAME,'plots')
 DATA_DIRs_NAMES = {dir_name: os.path.join(DATA_DIR_PATH, dir_name) for dir_name in __dirs}
 PICKLE_INTENSITY_INTERVALS_DIR = os.path.join(DATA_DIRs_NAMES['pickles'], 'intensity_intervals')
-BLM_DATA_DIR = DATA_DIRs_NAMES['blm_data_2017']
+BLM_DATA_DIR = DATA_DIRs_NAMES['blm_data']
 BLM_LIST_DIR = DATA_DIRs_NAMES['blm_lists']
 # BLM_FILES_REGEX_PATTERN = re.compile(
 #     r".*\/([\w\-]+)_(\d{4}(?:_?)\d{2}(?:_?)\d{2})\d*_(\d{4}(?:_?)\d{2}(?:_?)\d{2}).*(LOSS_RS\d\d|DOSE_INT_HH)\.(?:p|pkl)")
